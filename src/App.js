46739d4adab767895db5a2edfb8b95312bc73997
Abhishek <ak1824871@gmail.com>
2024-07-20 22:55:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="font-bold text-green-400 text-xl">
+      Let's build something ineteresting !!
+    </div>
+  );
+}
+
+export default App;
