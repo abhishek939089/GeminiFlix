9880a927de237bd5856d8747e0338c257cede74a
Abhishek <ak1824871@gmail.com>
2024-08-02 13:16:44 +0530

    TMDB data fetch & built Main Container

--- README.md
+++ README.md
@@ -16,3 +16,16 @@
 - Created Redux Store with userSlice
 - Implemented Sign Out
 - Update Profile
+- Bugfix: Sign up user displayName and profile picture update
+- BUgfix: if the user is not logged in redirect /browse and vice - versa
+- Add all the hardcoded values in a constants file
+- Register on TMDB API & create an app and get access token
+- Get data from tmdb nowPlayingMovie API
+- Custum hook for now Playing Movies
+- Create movieSlice
+- Update store with movies data
+- Made Main container and secondary container
+- Fetch data for Trailer video
+- Update store with trailor video
+- Embedded the yt video and make it autoplay and mute
+- Then add some css to make main container look awesome
---
+++ src/utils/constants.js
@@ -0,0 +1,13 @@
+export const LOGO =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const USER_AVATAR =
+  "https://occ-0-3752-3646.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABStlS0MPUGcy6Ovyeia-3ddnnXNb2Lri4P4H4QCFuR_yaGs0umyqHUDOZcOBKF8MFUGHX07txAW70z7wq_S9AKGQ_MixrLQ.png?r=a4b";
+
+export const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMTYyZTYxMWFmNjVmOTUxMWI4MjZmMzM4YWZiMGMyZiIsIm5iZiI6MTcyMjQwODU4MS42Mjk2MzQsInN1YiI6IjY2YTlkZDMzYzczMDFmOGE0Nzc1NjkwOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kkk3nucxi1o46h9v_ACOcGeUOqizbYI5TB0MI_fq36Y",
+  },
+};
